58eebc20dc77787d1dba065229275e0b42cf97eb
jmnote <opcore@gmail.com>
2024-01-27 04:40:10 +0000

    .

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func scrape(endpoint string) (content string, err error) {
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
 
 func main() {
-	fmt.Println("hello")
+	endpoints := []string{"https://kubernetespodcast.com/"}
+	for _, endpoint := range endpoints {
+		content, err := scrape(endpoint)
+		if err != nil {
+			fmt.Println("Error scraping:", err)
+			continue
+		}
+		fmt.Println(content)
+	}
 }
